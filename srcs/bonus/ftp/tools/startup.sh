3e67e93d1970c49ca525b5bd70060835d1e14565
tkaragoz <tkaragoz@student.42.fr>
2025-01-06 16:54:06 +0100

    ftp server is done

--- srcs/bonus/ftp/tools/startup.sh
+++ srcs/bonus/ftp/tools/startup.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
-# if [ ! -f "/etc/vsftpd.conf" ]; then
+mkdir -p /var/run/vsftpd/empty /var/www/html
 
-    echo "Initializing vsftpd configuration..."
+mv /tmp/vsftpd.conf /etc/vsftpd.conf
 
-    # Güvenli chroot dizini oluşturma
-    mkdir -p /var/run/vsftpd/empty
-    mkdir -p /var/www/html
-    mv /tmp/vsftpd.conf /etc/vsftpd.conf
+adduser --disabled-password --gecos "" "$FTP_USER"
+echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
+chown -R "$FTP_USER:$FTP_USER" /var/www/html
+chmod 755 /var/www/html
 
-    # vsftpd yapılandırma dosyasını yedekleme ve yeni yapılandırmayı uygulama
-    mv /tmp/vsftpd.conf /etc/vsftpd.conf
-
-    # FTP kullanıcısını oluşturma ve parola ayarlama
-    adduser --disabled-password --gecos "" "$FTP_USER"
-    echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
-
-    # /home/$FTP_USER dizininin sahipliğini ve izinlerini ayarlama
-    chown -R "$FTP_USER:$FTP_USER" /var/www/html
-    chmod 755 /var/www/html
-
-    # FTP kullanıcısını vsftpd kullanıcı listesine ekleme
-    echo "$FTP_USER" | tee -a /etc/vsftpd.userlist > /dev/null
-
-# fi
-
-echo "FTP server starting on port 21 and passive ports 20000-20100..."
+echo "$FTP_USER" | tee -a /etc/vsftpd.userlist > /dev/null
 
 exec "$@"
